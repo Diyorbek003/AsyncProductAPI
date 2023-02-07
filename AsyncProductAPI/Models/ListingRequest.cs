6f9e690ce4179b45c2e23456a3f1c8c8bec7f22a
Diyorbek003 <jaloldinusmonov609@gmail.com>
2023-02-08 01:18:01 +0500

    Add project files.

---
+++ AsyncProductAPI/Models/ListingRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncProductAPI.Models;
+
+public class ListingRequest
+{
+    public int Id { get; set; }
+    public string? RequestBody { get; set; }
+    public string? EstimatedCompentionTime { get; set; }
+    public string? RequestStatus { get; set; }
+    public string RequestId { get; set; } = Guid.NewGuid().ToString("N")[..10];
+
+}
